008e376eeb4084134e0df9730c896c424c94a494
Sandeep Shrivastava <sandeepshrivastava518@gmail.com>
2020-09-22 20:52:59 +0530

    Project Structure

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {}
